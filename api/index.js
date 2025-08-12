cb73159971931b9d5ce5693177812d351bfefdd2
pitikovd-creator <pitikovd@gmail.com>
2025-08-12 23:22:24 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
+  if (req.method !== "GET") return res.status(405).json({ error: "Method Not Allowed" });
   res.status(200).send("WB Bridge is alive");
 }
